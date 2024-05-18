67f30299a154aadf0fab3e06fb93f1e98dff3b36
Saman Rohanizade <saman.rohanizade@gmail.com>
2024-05-18 15:46:26 +0330

    complete the page

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+const minimumWaitPr = new Promise(resolve => setTimeout(resolve, 3000));
+
+minimumWaitPr.then(revealContent);
+
+function revealContent() {
+  const mask = document.getElementById('mask');
+  const maskContent = document.getElementById('mask-content');
+  const unRevealedPath = Snap.select('#revealed');
+  const revealedPath = Snap.select('#un-revealed');
+  const revealedPathPoints = revealedPath.node.getAttribute('d');
+  const reveal = () => {
+    unRevealedPath.animate({ d: revealedPathPoints }, 4300, mina.backout);
+  };
+  reveal();
+
+  maskContent.classList.add('slide-up');
+  maskContent.addEventListener('transitionend', () => {
+    mask.remove();
+  });
+}
